174008879165af1a1ee66788b03148d0267604c2
redhat14k <113951432+redhat14k@users.noreply.github.com>
2022-09-23 18:17:20 -0400

    Update folder.c

--- folder.c
+++ folder.c
@@ -1,45 +1,24 @@
-* program to copy data from one file to another file*/
-
-#include<studio.h>
-
+#include<stdio.h>
 #include<conio.h>
 
-void main()
-
+int main(int argc, char* argv[])
 {
-
 FILE *fp1,*fp2;
-
 char ch;
-
 clrscr();
-
-fp1=fopen(“mslifelineway.c”, “r”);
-
-fp2=fopen(“bakait.c”, “w”);
-
+fp1=fopen(argv[1], “r”);
+fp2=fopen(argv[2], “w”);
 while(1)
-
 {
-
 ch=fgetc(fp1);
-
-if(ch=EOF)
-
+if(ch=EOF){
 break;
-
-else
-
-putc(ch,fp2);
-
 }
-
+else
+putc(ch,fp2);
+}
 printf(“\nFile copied successfully!”);
-
 fclose(fp1);
-
 fclose(fp2);
-
 getch();
-
 }
