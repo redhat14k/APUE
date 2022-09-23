998e4a2cc0035f8948f455d492d150f0011b680c
redhat14k <113951432+redhat14k@users.noreply.github.com>
2022-09-23 17:10:32 -0400

    Update bbcp.c

--- bbcp.c
+++ bbcp.c
@@ -29,9 +29,9 @@ int main(int argc, char* argv[]){
         printf("error: %s\n", strerror(errno));
     }
 
-    if(argv[1] == argv[2]){
-	printf("error: %s\n", strerror(errno));
-    }
+    if (strcmp(argv[1], argv[2]) == 0) {
+	printf("Error: %s\n", strerror(errno));
+		exit(1);
 /*opens source file in read-only*/
     fin=open(argv[1], O_RDONLY);
     if(fin==-1){
