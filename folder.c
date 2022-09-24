5fc966c4d89820f41e3dcb3bca8f52bafeef173f
redhat14k <113951432+redhat14k@users.noreply.github.com>
2022-09-23 20:35:19 -0400

    Update folder.c

--- folder.c
+++ folder.c
@@ -1,24 +1,33 @@
-#include<stdio.h>
-#include<conio.h>
-
-int main(int argc, char* argv[])
+/*
+ * C Program to Copy a File into Another File
+ */
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+ 
+int main(int argc,char **argv)
 {
-FILE *fp1,*fp2;
-char ch;
-clrscr();
-fp1=fopen(argv[1], “r”);
-fp2=fopen(argv[2], “w”);
-while(1)
-{
-ch=fgetc(fp1);
-if(ch=EOF){
-break;
-}
-else
-putc(ch,fp2);
-}
-printf(“\nFile copied successfully!”);
-fclose(fp1);
-fclose(fp2);
-getch();
+    FILE *fp1, *fp2;
+    char ch;
+    int pos;
+    fp1 = fopen(argv[1],"r"
+    fp2 = fopen(argv[2], "w");  
+    if (!fp1 || !fp2 || !argc == 3){
+        printf("error: %s\n",strerror(2))
+    }
+    fseek(fp1, 0L, SEEK_END); // file pointer at end of file
+    pos = ftell(fp1);
+    fseek(fp1, 0L, SEEK_SET); // file pointer set at start
+    while (pos--)
+    {
+        ch = fgetc(fp1);  // copying file character by character
+        fputc(ch, fp2);
+    }    
+    fclose(fp1);
+    fclose(fp2);
 }
